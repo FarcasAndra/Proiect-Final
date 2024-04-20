97edbae11b07a02d045a0087a51fe8528d4df383
FarcasAndra <farcas.andra.carina@gmail.com>
2024-04-20 22:17:02 +0300

    add

--- signupSection.js
+++ signupSection.js
@@ -1,6 +1,7 @@
 /*Button Sign up*/
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("signupLink").addEventListener("click", function() {
+    document.getElementById("signupSection").addEventListener("submit", function(event) {
+        event.preventDefault();
         window.location.href = "signupSection.html";
     });
 });
